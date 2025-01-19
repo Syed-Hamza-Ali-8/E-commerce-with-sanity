54739f5e7c3395cdd9fde60e2b831d7e32650b62
Syed-Hamza-Ali-8 <ha5755420@gmail.com>
2025-01-19 19:18:41 +0500

    Day 1 , 2 and 3 completed

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-16",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
